cbad7c6e5d4b4d96f4f8102d3b37b7fae2b2c87c
Rahul Rawat <rr904196@gmail.com>
2025-04-27 15:02:14 +0530

    feat(mail): implement MailController, MailService, and MailModule with email sending functionality

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+import { MailController } from './mail.controller';
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: process.env.SMTP_HOST,
+          port: +process.env.SMTP_PORT,
+          secure: false,
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASSWORD,
+          },
+          tls: {
+            rejectUnauthorized: false,
+          },
+        },
+        defaults: {
+          from: process.env.FROM,
+        },
+        template: {
+          dir: __dirname + '/../templates',
+          adapter: new PugAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
+  controllers: [MailController],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
